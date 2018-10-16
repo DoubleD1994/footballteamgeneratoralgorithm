5cc9748499cebf7e174e8a10404e993e3c55a9ce
David Dryburgh <daviddryburgh@Davids-MacBook-Air.local>
2018-10-16 22:54:43 +0100

    Algorithm generates fair teams test created. fails initially.

--- src/test/java/com/dryburghsoftware/footballteamsalgorithm/algorithm/TestFootballTeamSelectionAlgorithm.java
+++ src/test/java/com/dryburghsoftware/footballteamsalgorithm/algorithm/TestFootballTeamSelectionAlgorithm.java
@@ -9,7 +9,9 @@ import org.junit.Test;
 
 import com.dryburghsoftware.footballteamsalgorithm.players.GroupOfPlayers;
 import com.dryburghsoftware.footballteamsalgorithm.players.Player;
+import com.dryburghsoftware.footballteamsalgorithm.teams.OverallCalculator;
 import com.dryburghsoftware.footballteamsalgorithm.teams.Team;
+import com.dryburghsoftware.footballteamsalgorithm.teams.TeamOverallCalculator;
 
 public class TestFootballTeamSelectionAlgorithm {
 
@@ -17,23 +19,39 @@ public class TestFootballTeamSelectionAlgorithm {
 	private FairTeamSorter fairTeamAlgorithm;
 	private Player john;
 	private Player steve;
+	private OverallCalculator teamOverallCalculator;
 	
 	@Before
 	public void setUp() throws Exception {
 		groupOfPlayers = new GroupOfPlayers();
 		fairTeamAlgorithm = new FairTeamAlgorithm();
-		john  = new Player("John", 20, 20, 20, 20, 20);
-		steve = new Player("Steve", 20, 20, 20, 20, 20);
-		groupOfPlayers.addPlayerToPlayingGroup(john);
-		groupOfPlayers.addPlayerToPlayingGroup(new Player("Bob", 10, 10, 10, 10, 10));
-		groupOfPlayers.addPlayerToPlayingGroup(steve);
-		groupOfPlayers.addPlayerToPlayingGroup(new Player("Rob", 10, 10, 10, 10, 10));		
+		teamOverallCalculator = new TeamOverallCalculator();
+		
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("A", 20, 20, 20, 20, 20));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("B", 15, 15, 15, 15, 15));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("C", 20, 20, 20, 20, 20));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("D", 10, 10, 10, 10, 10));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("E", 10, 10, 10, 10, 10));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("F", 15, 15, 15, 15, 15));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("G", 5, 5, 5, 5, 5));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("H", 1, 1, 1, 1, 1));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("I", 5, 5, 5, 5, 5));
+		groupOfPlayers.addPlayerToPlayingGroup(new Player("J", 1, 1, 1, 1, 1));
 	}
 
 	@Test
 	public void test_AlgorithmReturnsTwoTeams() {
 		List<Team> theTeams = fairTeamAlgorithm.getFairTeams(groupOfPlayers);
 		assertTrue(theTeams.size() == 2);
+	}
+	
+	@Test
+	public void test_AlgorithmCanGenerateFairTeams() {
+		List<Team> theTeams = fairTeamAlgorithm.getFairTeams(groupOfPlayers);
+		Team teamOne = theTeams.get(0);
+		Team teamTwo = theTeams.get(1);
+		boolean result = teamOverallCalculator.calculateTeamOverall(teamOne) == teamOverallCalculator.calculateTeamOverall(teamTwo);
+		assertTrue(result);
 	}
 	
 }
